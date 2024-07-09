ac128a4911463ac6436f70d0ba399801d83f84d2
Burak Gonca <goncabrk@gmail.com>
2024-07-09 16:58:56 +0300

    Personal-webpage React projesi olusturuldu

--- JS/Uygulama5Asenkron.js
+++ JS/Uygulama5Asenkron.js
@@ -1,23 +1,23 @@
 // get request
 
-const jsonURL = "https://jsonplaceholder.typicode.com/posts";
-const getReq = (url) => {
-    const xhr = new XMLHttpRequest()
-    xhr.open("GET", url)
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-              const response = JSON.parse(xhr.responseText);
-              console.log(response);
-            } else {
-              console.log(`${xhr.status} - Couldn't fetch the data`);
-            }
-          }
-    }
-    xhr.send()
-}
+// const jsonURL = "https://jsonplaceholder.typicode.com/posts";
+// const getReq = (url) => {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open("GET", url)
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState === 4) {
+//             if (xhr.status === 200) {
+//               const response = JSON.parse(xhr.responseText);
+//               console.log(response);
+//             } else {
+//               console.log(`${xhr.status} - Couldn't fetch the data`);
+//             }
+//           }
+//     }
+//     xhr.send()
+// }
 
-getReq(jsonURL)
+// getReq(jsonURL)
 
 
 
@@ -83,3 +83,100 @@ getReq(jsonURL)
 // xhr.setRequestHeader("Content-type","application/json; chartset=utf-8");
 // xhr.responseType="json";
 // xhr.send(newPost);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+
+// Aşağıda tarifinden anlayacağınız istekleri https://jsonplaceholder.typicode.com/ URL'ine atın. her bir isteği 1 kere then, 1 kere de async/await yöntemi ile atın. isteklerinizi fonksiyon içerisinde tanımlayın.
+//getCommentById, getAllComments, postComment, deleteComment, updateCommentPatch, updateCommentPut
+
+
+
+
+ 
+// const getCommentById = ((id) => {
+//   const jsonUrl = "https://jsonplaceholder.typicode.com/comments";
+//   fetch(`${jsonUrl}/${id}`)
+//       .then((res) => res.json())
+//       .then((data) => console.log(data));
+// })
+// getCommentById(3);
+
+
+
+//-------------------------------------------------
+
+
+// const getAllComments = (() =>{
+//   const jsonUrl = "https://jsonplaceholder.typicode.com/comments";
+//   fetch(`${jsonUrl}`)
+//   .then((response) =>response.json())
+//   .then((data) => console.log(data));
+
+// }
+// )
+
+// getAllComments();
+
+
+
+
+//-------------------------------------------------
+
+
+
+
+// const newComment = {
+//   name: "Burak Gonca",
+//   email: "burak@gonca.com",
+//   body: "lorem ipsum dolor sit amet non elit"
+
+// };
+
+// const postComment =((newComment)=>{
+//   const jsonUrl = "https://jsonplaceholder.typicode.com/comments";
+
+//   fetch(jsonUrl, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(newComment),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => console.log(data));
+
+// }
+// )
+
+// postComment(newComment);
+
+
+
+
+
+//-------------------------------------------------
+
+
+
+
+
+
+
